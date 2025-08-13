62fcef3323194061077557058c1ee9584b8a627e
dipak1920basnet <dipak1920basnet@gmail.com>
2025-08-14 00:12:56 +0545

    gives a random color on hover

--- script_files/script.js
+++ script_files/script.js
@@ -1,16 +1,26 @@
 let default_size = 16
+function getRandomInt(min, max) {
+  min = Math.ceil(min);   // round up
+  max = Math.floor(max);  // round down
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 document.addEventListener("DOMContentLoaded",()=>{
     set_grid = document.getElementById("set-grid")
     set_grid.addEventListener("click",()=>{
         let grid_size = prompt("Enter the grid size from 16 to 100");
-        if(grid_size >= default_size && grid_size < 100)
+        if(grid_size >= default_size && grid_size <= 100)
         {
             default_size = grid_size;
             // make ground for different size
             document.getElementById("draw-box").innerHTML = null;
             make_ground()
         }
-        console.log(default_size)
+        // console.log(default_size)
+        else{
+            document.getElementById("draw-box").innerHTML = null;
+            make_ground()
+        }
     })
 
 
@@ -46,15 +56,18 @@ document.addEventListener("DOMContentLoaded",()=>{
             // element.style.backgroundColor = "white";
             // Add hover effects 
             col_grid_box.addEventListener("mouseenter",()=>{
-                if (col_grid_box.style.backgroundColor != "green")
-                {
-                    col_grid_box.style.backgroundColor = "rgb(0, 255, 0)";
-                }
-                else
-                {
-                    col_grid_box.style.backgroundColor = "rgb(255, 255, 255)";
-                }
-                
+                // if (col_grid_box.style.backgroundColor != "green")
+                // {
+                //     col_grid_box.style.backgroundColor = "rgb(0, 255, 0)";
+                // }
+                // else
+                // {
+                //     col_grid_box.style.backgroundColor = "rgb(255, 255, 255)";
+                // }
+                let red = getRandomInt(0,255);
+                let green = getRandomInt(0,255);
+                let blue = getRandomInt(0,255);
+                col_grid_box.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
                 })
             element.appendChild(col_grid_box) 
             }
